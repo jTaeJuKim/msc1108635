809ba88b5000a20b29b8dd26bef5d07e6b29c736
Justin T Kim <tjk.abdn@gmail.com>
2012-06-04 21:00:08 +0100

    Initial set up with testing env

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Msc::Application.config.secret_token = 'f66bbac0b83a5dd75340817af3f77834a868533cee13a4dabb7b21f258ee99ee6e3addcf5755817f72e2d71bb9e204aacb6ab5591f636625c80c527f8b033747'
